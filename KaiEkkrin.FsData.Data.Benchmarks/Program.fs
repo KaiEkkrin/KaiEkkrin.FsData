d99ad8802729c1f13e5acab7519fc2f477407a18
Alex Holloway <alex.holloway.ctx@gmail.com>
2023-07-14 23:10:51 +0100

    Basic benchmarks

---
+++ KaiEkkrin.FsData.Data.Benchmarks/Program.fs
@@ -0,0 +1,128 @@
+﻿open System
+open System.Collections.Generic
+open System.Collections.Immutable
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open KaiEkkrin.FsData.Data
+
+// See https://phillipcarter.dev/posts/benchmarking-fsharp/
+
+// TODO try using other, more complicated keys and values here.
+// For now I'm going to use a string key and this value type:
+type TestValue = {
+    Num1: int
+    Num2: int
+    Num3: int
+}
+
+[<MemoryDiagnoser>]
+type IbpTree2Bench() =
+    let random = new Random(598237459)
+    let (buf: byte[]) = Array.zeroCreate 16
+    let genRandomKeyValue _ =
+        random.NextBytes buf
+        let key = Convert.ToBase64String buf
+        let value = { Num1 = random.Next(); Num2 = random.Next(); Num3 = random.Next() }
+        KeyValuePair (key, value)
+
+    let randomKeyValues = Array.init 1000 genRandomKeyValue
+    let lotsMoreRandomKeyValues = Array.init 100_000 genRandomKeyValue
+
+    let randomKeysToDelete = Array.init 1000 <| fun i ->
+        let index = random.Next (0, 99_999)
+        lotsMoreRandomKeyValues[index].Key
+    
+    let largeTree3 = IbpTree2.createFromB (3, StringComparer.Ordinal, lotsMoreRandomKeyValues)
+    let largeTree16 = IbpTree2.createFromB (16, StringComparer.Ordinal, lotsMoreRandomKeyValues)
+
+    let largeIsd =
+        let builder = ImmutableSortedDictionary<string, TestValue>.Empty.WithComparers(StringComparer.Ordinal).ToBuilder()
+        for kv in lotsMoreRandomKeyValues do
+            builder[kv.Key] <- kv.Value
+
+        builder.ToImmutable()
+
+    [<Benchmark(Description = "Create IbpTree2(3) from 1000 random values")>]
+    member this.CreateIbp3() =
+        IbpTree2.createFromB (3, StringComparer.Ordinal, randomKeyValues)
+
+    [<Benchmark(Description = "Create IbpTree2(16) from 1000 random values")>]
+    member this.CreateIbp16() =
+        IbpTree2.createFromB (16, StringComparer.Ordinal, randomKeyValues)
+
+    [<Benchmark(Description = "Create ImmutableSortedDictionary from 1000 random values")>]
+    member this.CreateIsd() =
+        let builder = ImmutableSortedDictionary<string, TestValue>.Empty.WithComparers(StringComparer.Ordinal).ToBuilder()
+        for kv in randomKeyValues do
+            builder[kv.Key] <- kv.Value
+
+        builder.ToImmutable()
+
+    [<Benchmark(Description = "Insert 1000 random values into empty IbpTree2(3)")>]
+    member this.InsertEmptyIbp3() =
+        let emptyTree = IbpTree2.createB (3, StringComparer.Ordinal)
+        randomKeyValues |> Array.fold (fun t kv -> IbpTree2.insert kv.Key kv.Value t) emptyTree
+
+    [<Benchmark(Description = "Insert 1000 random values into empty IbpTree2(16)")>]
+    member this.InsertEmptyIbp16() =
+        let emptyTree = IbpTree2.createB (16, StringComparer.Ordinal)
+        randomKeyValues |> Array.fold (fun t kv -> IbpTree2.insert kv.Key kv.Value t) emptyTree
+
+    [<Benchmark(Description = "Insert 1000 random values into empty ImmutableSortedDictionary")>]
+    member this.InsertEmptyIsd() =
+        let emptyTree = ImmutableSortedDictionary<string, TestValue>.Empty.WithComparers(StringComparer.Ordinal)
+        randomKeyValues |> Array.fold (fun (t: ImmutableSortedDictionary<string, TestValue>) kv -> t.SetItem (kv.Key, kv.Value)) emptyTree
+
+    [<Benchmark(Description = "Insert 1000 random values into large IbpTree2(3)")>]
+    member this.InsertLargeIbp3() =
+        randomKeyValues |> Array.fold (fun t kv -> IbpTree2.insert kv.Key kv.Value t) largeTree3
+
+    [<Benchmark(Description = "Insert 1000 random values into large IbpTree2(16)")>]
+    member this.InsertLargeIbp16() =
+        randomKeyValues |> Array.fold (fun t kv -> IbpTree2.insert kv.Key kv.Value t) largeTree16
+
+    [<Benchmark(Description = "Insert 1000 random values into large ImmutableSortedDictionary")>]
+    member this.InsertLargeIsd() =
+        randomKeyValues |> Array.fold (fun (t: ImmutableSortedDictionary<string, TestValue>) kv -> t.SetItem (kv.Key, kv.Value)) largeIsd
+
+    [<Benchmark(Description = "Delete 1000 random values from large IbpTree2(3)")>]
+    member this.DeleteLargeIbp3() =
+        randomKeysToDelete |> Array.fold (fun t k -> IbpTree2.delete k t) largeTree3
+
+    [<Benchmark(Description = "Delete 1000 random values from large IbpTree2(16)")>]
+    member this.DeleteLargeIbp16() =
+        randomKeysToDelete |> Array.fold (fun t k -> IbpTree2.delete k t) largeTree16
+
+    [<Benchmark(Description = "Delete 1000 random values from large ImmutableSortedDictionary")>]
+    member this.DeleteLargeIsd() =
+        randomKeysToDelete |> Array.fold (fun (t: ImmutableSortedDictionary<string, TestValue>) k -> t.Remove k) largeIsd
+
+    [<Benchmark(Description = "Enumerate the first 1000 values from large IbpTree2(3)")>]
+    member this.EnumerateStartLargeIbp3() =
+        largeTree3 |> Seq.take 1000 |> Seq.last
+
+    [<Benchmark(Description = "Enumerate the first 1000 values from large IbpTree2(16)")>]
+    member this.EnumerateStartLargeIbp16() =
+        largeTree16 |> Seq.take 1000 |> Seq.last
+
+    [<Benchmark(Description = "Enumerate the first 1000 values from large ImmutableSortedDictionary")>]
+    member this.EnumerateStartLargeIsd() =
+        largeIsd |> Seq.take 1000 |> Seq.last
+
+    [<Benchmark(Description = "Enumerate 1000 values from within a large IbpTree2(3)")>]
+    member this.EnumerateMidLargeIbp3() =
+        largeTree3 |> IbpTree2.enumerateFrom (randomKeysToDelete[0]) |> Seq.take 1000 |> Seq.last
+
+    [<Benchmark(Description = "Enumerate 1000 values from within a large IbpTree2(16)")>]
+    member this.EnumerateMidLargeIbp16() =
+        largeTree16 |> IbpTree2.enumerateFrom (randomKeysToDelete[0]) |> Seq.take 1000 |> Seq.last
+
+    [<Benchmark(Description = "Enumerate 1000 values from within a large ImmutableSortedDictionary")>]
+    member this.EnumerateMidLargeIsd() =
+        largeIsd |> Seq.filter (fun kv -> kv.Key < randomKeysToDelete[0]) |> Seq.take 1000 |> Seq.last
+
+[<EntryPoint>]
+let main argv =
+    BenchmarkRunner.Run<IbpTree2Bench>() |> ignore
+    0
+
